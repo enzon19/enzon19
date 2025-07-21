e5cac48deb035902b6b2b02abc8452590149b634
Enzo <enzonbarata@outlook.com>
2025-07-21 00:43:06 -0300

    fix: break on selectLanguageElement not available

--- js/translation.js
+++ js/translation.js
@@ -1,11 +1,15 @@
-const selectLanguageElement = document.querySelector('#selectLanguage');
+const selectLanguageElement = document.querySelector("#selectLanguage");
 
 function handleLanguageChange() {
-  window.location.search = 'lng=' + selectLanguageElement.value;
+  if (selectLanguageElement)
+    window.location.search = "lng=" + selectLanguageElement.value;
 }
 
 function loadLanguage() {
-  selectLanguageElement.value = ['pt', 'eng'].find(lng => lng == window.i18nextify.i18next.language) || '';
+  if (selectLanguageElement)
+    selectLanguageElement.value =
+      ["pt", "eng"].find((lng) => lng == window.i18nextify.i18next.language) ||
+      "";
 }
 
-window.i18nextify.i18next.on('initialized', loadLanguage);
+window.i18nextify.i18next.on("initialized", loadLanguage);
