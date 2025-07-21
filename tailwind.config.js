55e83a15164a583770fa17b6c4e133f33b34ef40
Enzo <enzonbarata@outlook.com>
2025-07-21 00:21:19 -0300

    feat: donate page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./trakt-tools/*.html"],
+  content: ["./*.html", "./trakt-tools/*.html", "./donate/*.html"],
   theme: {
     fontFamily: {
-      'sans': ['Inter', 'sans-serif']
+      sans: ["Inter", "sans-serif"],
     },
     extend: {
       colors: {
-        'enzo-sky': '#0080b7',
-        'neutral-850': '#333333'
+        "enzo-sky": "#0080b7",
+        "neutral-850": "#333333",
       },
       boxShadow: {
-        'enzo-shadow-size': '0px 0px 50px -12px rgb(0 0 0 / 0.25)'
+        "enzo-shadow-size": "0px 0px 50px -12px rgb(0 0 0 / 0.25)",
         //          offset-x | offset-y | blur-radius | spread-radius | color
       },
       screens: {
-        'xs': '415px'
-      }
-    }
+        xs: "415px",
+      },
+    },
   },
-  darkMode: 'class',
+  darkMode: "class",
   plugins: [],
-}
-
+};
