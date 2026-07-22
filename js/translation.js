63166f68efc239793712e6db9f6729ba9619a796
Enzo <enzonbarata@outlook.com>
2026-07-21 22:35:50 -0300

    style: prettier

--- js/translation.js
+++ js/translation.js
@@ -1,15 +1,15 @@
-const selectLanguageElement = document.querySelector("#selectLanguage");
+const selectLanguageElement = document.querySelector('#selectLanguage');
 
 function handleLanguageChange() {
-  if (selectLanguageElement)
-    window.location.search = "lng=" + selectLanguageElement.value;
+	if (selectLanguageElement)
+		window.location.search = 'lng=' + selectLanguageElement.value;
 }
 
 function loadLanguage() {
-  if (selectLanguageElement)
-    selectLanguageElement.value =
-      ["pt", "eng"].find((lng) => lng == window.i18nextify.i18next.language) ||
-      "";
+	if (selectLanguageElement)
+		selectLanguageElement.value =
+			['pt', 'eng'].find((lng) => lng == window.i18nextify.i18next.language) ||
+			'';
 }
 
-window.i18nextify.i18next.on("initialized", loadLanguage);
+window.i18nextify.i18next.on('initialized', loadLanguage);
--- js/writeName.js
+++ js/writeName.js
@@ -1,55 +1,59 @@
-function shuffleArray (array) {
-  let currentIndex = array.length, randomIndex;
+function shuffleArray(array) {
+	let currentIndex = array.length,
+		randomIndex;
 
-  while (currentIndex != 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
+	while (currentIndex != 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex],
+			array[currentIndex],
+		];
+	}
 
-  return array;
+	return array;
 }
 
 const nameElement = document.querySelector('#name');
 
 async function type() {
-  if (nameElement.classList.contains('running')) return;
-  
-  const nicknames = ['Enzão', 'Barata', 'Baratinha'];
-  let shuffledNicknames = shuffleArray(nicknames);
-  shuffledNicknames.push('Enzo', 'enzon19');
+	if (nameElement.classList.contains('running')) return;
 
-  nameElement.classList.add('running');
-  for (let index = 0; index < shuffledNicknames.length; index++) {    
-    const text = shuffledNicknames[index];
-    await deleteText();
-    await changeText(text);
-    if (index === shuffledNicknames.length - 1) nameElement.classList.remove('running');
-  }
+	const nicknames = ['Enzão', 'Barata', 'Baratinha'];
+	let shuffledNicknames = shuffleArray(nicknames);
+	shuffledNicknames.push('Enzo', 'enzon19');
+
+	nameElement.classList.add('running');
+	for (let index = 0; index < shuffledNicknames.length; index++) {
+		const text = shuffledNicknames[index];
+		await deleteText();
+		await changeText(text);
+		if (index === shuffledNicknames.length - 1)
+			nameElement.classList.remove('running');
+	}
 }
 
 function deleteText() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      nameElement.classList.remove('type');
-      nameElement.classList.add('delete');
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			nameElement.classList.remove('type');
+			nameElement.classList.add('delete');
 
-      resolve(true);
-    }, 1700);
-  });
+			resolve(true);
+		}, 1700);
+	});
 }
 
 function changeText(text) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      nameElement.querySelector('h1').innerText = text;
-      nameElement.classList.remove('delete');
-      nameElement.classList.add('type');
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			nameElement.querySelector('h1').innerText = text;
+			nameElement.classList.remove('delete');
+			nameElement.classList.add('type');
 
-      resolve(true);
-    }, 1000);
-  });
+			resolve(true);
+		}, 1000);
+	});
 }
 
 type();
