44aa55158124323f9c5e1e9349d5fd374e63c6f7
Enzo <enzonbarata@outlook.com>
2026-07-22 21:13:47 -0300

    feat: hero shader background

---
+++ js/heroBackground.js
@@ -0,0 +1,33 @@
+import {
+	ShaderMount,
+	meshGradientFragmentShader,
+	getShaderColorFromString,
+} from '@paper-design/shaders';
+
+const container = document.querySelector('#hero');
+const shaderParams = {
+	u_colors: [
+		getShaderColorFromString('#28aefc'),
+		getShaderColorFromString('#286cff'),
+		getShaderColorFromString('#39aeec'),
+		getShaderColorFromString('#001c80'),
+	],
+	u_colorsCount: 4,
+	u_distortion: 0.4,
+	u_swirl: 0.15,
+	u_grainMixer: 0.1,
+	u_fit: 'contain',
+	u_scale: 1,
+	u_rotation: 0,
+	u_offsetX: 0.5,
+	u_offsetY: 0,
+};
+
+const speed = 1;
+const meshGradient = new ShaderMount(
+	container,
+	meshGradientFragmentShader,
+	shaderParams,
+	undefined,
+	speed,
+);
