dacded5f8ec55c9408b76b51766f53dcf7f4cae3
Enzo <enzonbarata@outlook.com>
2026-08-12 16:03:20 -0300

    feat: secret objects that make enzo follow

--- js/face-interactions.js
+++ js/face-interactions.js
@@ -42,3 +42,42 @@ if (
 } else {
 	setShakeListener();
 }
+
+// ----- FOOD -----
+const hasTouchscreen = 'ontouchstart' in window;
+let currentObjectCursor = null;
+if (!hasTouchscreen) {
+	const bg = document.querySelector('main > section#hero');
+	const face = document.querySelector('#enzo-face');
+
+	face.parentElement.addEventListener('mouseover', (event) => {
+		if (faceVariant != 'default') return;
+
+		const chance = Math.random();
+		if (chance > 0.03) return;
+
+		const objectOptions = [
+			'pizza',
+			'burger',
+			'chocolate',
+			'money',
+			'javascript',
+		];
+		const randomObjectIndex = Math.floor(Math.random() * objectOptions.length);
+
+		currentObjectCursor = objectOptions[randomObjectIndex];
+		faceVariant = randomObjectIndex >= 3 ? 'love' : 'yummy';
+		bg.style.cursor = `url(/assets/cursor/${currentObjectCursor}.png), auto`;
+	});
+
+	function resetCursor() {
+		if (faceVariant == 'dizzy') return;
+
+		bg.style.cursor = 'default';
+		faceVariant = 'default';
+		currentObjectCursor = null;
+	}
+
+	bg.addEventListener('mouseleave', resetCursor);
+	face.addEventListener('click', resetCursor);
+}
