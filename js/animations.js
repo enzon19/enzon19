a43fc88d16de3e8de2538925d03fdc0df115211d
Enzo <enzonbarata@outlook.com>
2026-08-13 19:21:02 -0300

    refactor: no effects in scroll smoother

--- js/animations.js
+++ js/animations.js
@@ -1,10 +1,11 @@
+// [ELEMENT] [SCREEN]
+
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 ScrollSmoother.create({
 	wrapper: '#smooth-wrapper',
 	content: '#smooth-content',
 	smooth: 1.5, // how long (in seconds) it takes to "catch up" to the native scroll position
-	effects: true, // looks for data-speed and data-lag attributes on elements
 	smoothTouch: 0.2, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
 });
 
