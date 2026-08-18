86a3a4c3548196097b901abb8fe01a8324746f52
Enzo <enzonbarata@outlook.com>
2026-08-18 13:39:41 -0300

    feat: fly up animation for text and elements

--- js/animations.js
+++ js/animations.js
@@ -29,6 +29,21 @@ gsap.to('#hero-content', {
 	},
 });
 
+const flyUpElements = gsap.utils.toArray('.gsap-fly-up');
+flyUpElements.forEach((elemento) => {
+	gsap.from(elemento, {
+		y: 50,
+		opacity: 0,
+		duration: 0.8,
+		ease: 'power3.out',
+		scrollTrigger: {
+			trigger: elemento,
+			start: 'top 96.7%',
+			toggleActions: 'play play none reverse', // [START] [QUICK START] [END TO START] [END]
+		},
+	});
+});
+
 const mm = gsap.matchMedia();
 mm.add(
 	{
