4c1d484e4c22dc0545fbefe76443dc14dc9dafa1
Enzo <enzonbarata@outlook.com>
2026-09-21 11:24:34 -0300

    refactor: fly up animation with stagger

--- js/animations/core.js
+++ js/animations/core.js
@@ -7,18 +7,26 @@ gsap.ticker.add((time) => {
 });
 gsap.ticker.lagSmoothing(0);
 
-const flyUpElements = gsap.utils.toArray('.gsap-fly-up');
-flyUpElements.forEach((e) => {
-	gsap.from(e, {
-		y: 50,
-		opacity: 0,
-		duration: 0.8,
-		filter: 'blur(0.5rem)',
-		ease: 'power3.out',
-		scrollTrigger: {
-			trigger: e,
-			start: 'top 96.7%',
-			toggleActions: 'play play none reverse', // [START] [QUICK START] [END TO START] [END]
-		},
-	});
+ScrollTrigger.batch('.gsap-fly-up', {
+	start: 'top 96.7%',
+	interval: 0.3,
+	onEnter: (batch) =>
+		gsap.to(batch, {
+			y: 0,
+			opacity: 1,
+			filter: 'blur(0rem)',
+			duration: 1.2,
+			delay: 0.05,
+			stagger: 0.2,
+			ease: 'power3.out',
+		}),
+	onLeaveBack: (batch) =>
+		gsap.to(batch, {
+			y: 50,
+			opacity: 0,
+			filter: 'blur(0.4rem)',
+			duration: 0.6,
+			stagger: 0.1,
+			ease: 'power3.out',
+		}),
 });
